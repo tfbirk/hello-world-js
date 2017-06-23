9141cbd7c04dc91e9f9885594cb391c4b61d1b41
Taylor Brown <taylor.fra.brown@hpe.com>
2017-06-23 14:09:08 -0500

    add MyFunc button

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,8 @@ var video = document.querySelector('video');
 var source = document.createElement('source');
 source.setAttribute('src', "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_640x360.m4v")
 video.appendChild(source);
+
+function myFunc()
+{
+    var myHeading = document.querySelector('h1').textContent = 'Hi, how are you?';
+}
