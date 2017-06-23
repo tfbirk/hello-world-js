df18bd072436847096b2af8adcbf5694a7d233c0
Taylor Brown <taylor.fra.brown@hpe.com>
2017-06-23 14:01:21 -0500

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Howdy world!';
+
+var video = document.querySelector('video');
+var source = document.createElement('source');
+source.setAttribute('src', "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_640x360.m4v")
+video.appendChild(source);
